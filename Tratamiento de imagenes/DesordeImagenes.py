0894a4846c8432c97acda6dc83c6f42fef3ebf67
David Peña <dfph1996@gmail.com>
2018-09-09 19:19:28 -0500

    Optimizacion codigo

--- Tratamiento de imagenes/DesordeImagenes.py
+++ Tratamiento de imagenes/DesordeImagenes.py
@@ -1,8 +1,26 @@
+'''
+Tratamiento de imagenes 
+Introduccion a la ciencia de los datos
+'''
+
 import numpy as np
 from skimage import io
-import os
-clear=lambda: os.system('clear')
+from random import shuffle
 
+img=io.imread('los-simpson-400x400.jpg',True)
 
-clear()
-img=io.imread('',True)
+#obtiene 16 pedazos de la imagen de 100*100 pixeles
+chunks = np.array([img[y:y+100,x:x+100] for x in [0,100,200,300] for y in [0,100,200,300]])
+    
+#horizontal stack
+positions = np.arange(16)
+np.random.shuffle(positions)
+Harray = []
+for col in range(0,16,4):
+    #une horizontalmente 4 pedazos
+    pos = positions[col:col+4]
+    hor = np.hstack((chunks[pos[0]],chunks[pos[1]],chunks[pos[2]],chunks[pos[3]]))
+    Harray.append(hor)
+#vertical stack 
+img_final = np.vstack((Harray[0],Harray[1],Harray[2],Harray[3]))    
+io.imsave('final.jpg',img_final)
